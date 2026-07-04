e0100ea57461ead1bcc7dc379124878f5cbbd54f
nathantwalton <52057421+nathantwalton@users.noreply.github.com>
2026-07-04 14:56:40 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom/client";
 import PointOMatic from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PointOMatic />
-  </React.StrictMode>
+  React.createElement(PointOMatic)
 );
